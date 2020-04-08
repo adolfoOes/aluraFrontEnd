eaddd909d153ea2057e2b1b6b70e94de821649ec
Adolfo <adolfo.oes@gmail.com>
2020-04-08 10:32:39 -0300

    typescript alura final part 1

--- alurabank/app/js/views/View.js
+++ alurabank/app/js/views/View.js
@@ -1,9 +1,9 @@
 class View {
     constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
     update(model) {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
     template(model) {
         throw new Error('Você deve implementar o método template!');
