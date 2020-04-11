e8c61290f6b3b8c908d0ad9c6b48d8b637228b0f
Adolfo <adolfo.oes@gmail.com>
2020-04-11 10:58:48 -0300

    alura angular creating a pipe to filter

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
@@ -10,7 +11,8 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: any[] = []; 
+  photos: Photo[] = []; 
+  filter: string = '';
   
   constructor(
     private photoService: PhotoService,
